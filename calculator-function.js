ecfab3ee61a1d7b193d60436ab6c409483073122
Sandra Grahl <grahlsan@gmail.com>
2018-10-22 18:07:42 +0100

    tests passed

--- calculator-function.js
+++ calculator-function.js
@@ -1,19 +1,28 @@
 
 
-let displayRes = "";
+let displayRes = "0";
 
 
 function buttonClickHandler(event) {
     const value = event.target.value;
     if (value === "all-clear") {
-        displayRes = "";
+        displayRes = "0";
     } else if (value === "=") {
-        displayRes = Function(`return ${displayRes}`)();
-        console.log(displayRes)
-    } else {
+        displayRes = String(Function(`return ${displayRes}`)());
+    } else if (value === "."){
+        let parts = displayRes.split(/[+\-/*]/);
+            if (parts[parts.length-1].indexOf(".") !== -1){
+        return
+        }
+        displayRes = displayRes + value; 
+    } else if (displayRes.match(/[+\-/*]$/) !== null && value.match(/[+\-/*]/) !== null){
+        displayRes = displayRes.slice(0,-1) + value;
+    } else if(displayRes === "0"){
+            displayRes = value;
+        } else {
         displayRes = displayRes + value;
     }
-    document.getElementById("calculator-screen").value = displayRes;
+    document.getElementById("display").value = displayRes;
 }
 
 const buttonElement = document.querySelectorAll('#calculator button');
